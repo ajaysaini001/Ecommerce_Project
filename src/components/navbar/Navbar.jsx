8fb7478c79599e0b29061b9e0bb24fcc8e0aa7e3
ajaysaini001 <123984045+ajaysaini001@users.noreply.github.com>
2024-02-08 12:49:36 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { CiCirclePlus } from "react-icons/ci";
+// import { CiCirclePlus } from "react-icons/ci";
 import "./style.css";
 const Navbar = () => {
   return (
@@ -9,7 +9,7 @@ const Navbar = () => {
                 <li className="logo">eCommerce</li>
                 <li className="li">Products</li>
                 <li className="li">Add a product </li>
-                <li className='plus'><CiCirclePlus size={30}/></li>
+                <li className='plus'>+</li>
             </ul>
             <div className="right">
                 <span className='name'>John Doe</span>
